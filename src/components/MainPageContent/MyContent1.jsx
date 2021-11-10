5425573830ece95eaefff9b2fa1fdb9bb62f62c7
BayelOsorov <osorov.bayel@gmail.com>
2021-11-10 22:34:31 +0600

    adaptive css

---
+++ src/components/MainPageContent/MyContent1.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+export default function StandardImageList() {
+    return (
+        <>
+            <div className='container' >
+                <div className='content1'>
+
+                    <div className='c1-text' >
+                        <h1>This is content</h1>
+                        <p>Konsept tasarım şıklığıyla yaşam alanlarında büyüleyici izler bırakan Area Mobilya’nın, her parçası son detayına kadar özenle hazırlanmış zarif modellerini keşfedin.</p>
+                    </div>
+                    <div className='c1-img' >
+                        <img src="https://areamobilya.com/wp-content/uploads/2020/08/home-koleksiyon-prada.jpg" />
+
+                        <img src="https://areamobilya.com/wp-content/uploads/2020/08/home-koleksiyon-lion.jpg" />
+
+                    </div>
+                    <div className='c1-img2' >
+                        <img src="https://areamobilya.com/wp-content/uploads/2020/08/home-koleksiyon-asus.jpg" />
+
+                        <img src="https://areamobilya.com/wp-content/uploads/2020/08/home-koleksiyon-lux-mimoza.jpg" />
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    );
+}
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MyHeader from '../components/MainPageContent/MyHeader';
+import MyContent1 from '../components/MainPageContent/MyContent1';
+import MyContent2 from '../components/MainPageContent/MyContent2';
+
+const MainPage = () => {
+    return (
+        <>
+            <MyHeader />
+            <MyContent1 />
+            <MyContent2 />
+        </>
+    );
+};
+
+export default MainPage;
